76f533daa8b9cf5905762b29f2765caff8cb3cf7
Anderson Matheus Lemke <amatheuslemke@hotmail.com>
2018-09-25 14:14:22 -0300

    Adding esm dependency and configuring mocha

---
+++ src/init/init.spec.js
@@ -0,0 +1,10 @@
+import chai from 'chai';
+
+describe('Array', function () {
+    describe('#indexOf()', function () {
+        it('should return -1 when the value is not present', function () {
+            let notPresentValue = [1, 2, 3].indexOf(4);
+            chai.expect(notPresentValue).to.equal(-1);
+        });
+    });
+});
